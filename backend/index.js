91ce3228ff35c6696efa60f3acd2028eaf369065
RenzoEspinoza <37278863+RenzoEspinoza@users.noreply.github.com>
2020-07-08 15:02:12 -0400

    Implemented a websocket using socket.io

--- backend/index.js
+++ backend/index.js
@@ -1,37 +1,39 @@
 const express = require('express')
 const cors = require('cors')
+const { callbackify } = require('util')
 
 const app = express()
 app.use(express.json())
 app.use(cors())
 app.use(express.static('build'))
+const server = require('http').createServer(app)
+const io = require('socket.io')(server)
 
 let songs = [
   {
     "id": "song-jYmHSg4GElhDEjcwZp3Tn",
-    "title": "1",
+    "title": "hello",
     "artist": "artist",
     "duration": "1:11",
-    "dateAdded": 1592941124347,
-    "voteCount": 1
+    "dateAdded": 1592941124347
   },
   {
     "id": "song-r06dKEZDE8uY9k4fQApGf",
-    "title": "2",
+    "title": "world",
     "artist": "artist",
     "duration": "2:22",
-    "dateAdded": 1592941128725,
-    "voteCount": 2
+    "dateAdded": 1592941128725
   },
   {
     "id": "song-ODs_wmon0Xweg0-d2cxxC",
-    "title": "3",
+    "title": 'hello world',
     "artist": "artist",
     "duration": "1:11",
-    "dateAdded": 1592941136073,
-    "voteCount": 3
+    "dateAdded": 1592941136073
   }
 ]
+
+let pool = []
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>')
@@ -41,9 +43,13 @@ app.get('/api/songs', (req, res) => {
   res.json(songs)
 })
 
-app.get('/api/songs/:id', (req, res) => {
-  const id = req.params.id
-  const song = songs.find(song => song.id === id)
+app.get('/api/pool', (req, res) => {
+  res.json(pool)
+})
+
+app.get('/api/songs/:query', (req, res) => {
+  const query = req.params.query
+  const song = songs.find(song => song.title === query)
   if (song) {res.json(song)} 
   else {res.status(404).end()}
 })
@@ -54,39 +60,62 @@ app.delete('/api/songs/:id', (req, res) => {
   res.status(204).end()
 })
 
-app.post('/api/songs', (req, res) => {
+app.post('/api/pool', (req, res) => {
   const body = req.body
-  
-  const duplicate = songs.some(song => song.id === body.id)
-  console.log('duplicate:', duplicate)
-  if(duplicate){
-    return res.status(400).json({
-      error: 'song already exists in pool'
-    })
-  }
-
-  const song = {
-    id: body.id,
-    title: body.title,
-    artist: body.artist,
-    duration: body.duration,
-    voteCount: 0,
-    dateAdded: Date.now()
-  }
-
-  songs = songs.concat(song)
-  res.json(song)
-})
-
-app.patch('/api/songs/:id', (req, res) => {
-  const id = req.params.id
-  songs = songs.map(song => {return (song.id === id) ? {...song, voteCount: (song.voteCount + req.body.vote)} : song})
+  addSong(body)
   res.status(204).end()
 })
 
-// fix the patch for updating votes and add sorting
+io.on('connection', socket => {
+  console.log('a user connected')
+  socket.on('song add', (song, callback) => {
+    const error = addSong(song)
+    if(error) return callback(error)
+    sortPool()
+    io.emit('pool update', pool)
+  })
+
+  socket.on('vote change', (id, vote) =>{
+    updateVote(id, vote)
+    sortPool()
+    io.emit('pool update', pool)
+  })
+})
+
+const addSong = song => {
+  console.log(pool)
+  const duplicate = pool.some(s => s.id === song.id)
+  console.log('duplicate:', duplicate)
+  if(duplicate){
+    return 'Error: song already exists in pool'
+  }
+
+  const newSong = {
+    id: song.id,
+    title: song.title,
+    artist: song.artist,
+    duration: song.duration,
+    voteCount: 0,
+    dateAdded: Date.now()
+  }
+  pool.push(newSong)
+}
+
+const updateVote = (id, vote) => {
+  pool = pool.map(song => {return (song.id === id) ? {...song, voteCount: (song.voteCount + vote)} : song})
+}
+
+const sortPool = () =>{
+  pool.sort(function(a,b) {
+    const voteA = a.voteCount
+    const voteB = b.voteCount
+    const dateA = a.dateAdded
+    const dateB = b.dateAdded
+    return (voteA > voteB) ? -1 : (voteA < voteB) ? 1 : (dateA > dateB) ? 1 : -1
+  })
+}
 
 const PORT = process.env.PORT || 3001
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,11 @@ import React, {useState, useEffect} from 'react'
 import SongSearch from './components/SongSearch'
 import SongPool from './components/SongPool'
 import {PoolSong, SearchResultSong} from './components/Song'
-import {nanoid} from 'nanoid'
 import axios from 'axios'
+import io from 'socket.io-client'
 
-const baseUrl = 'http://localhost:3001/api/songs'
-
+const baseUrl = 'http://localhost:3001/api/'
+let socket;
 
 const App = () => {
   const [poolList, setPoolList] = useState([])
@@ -14,51 +14,37 @@ const App = () => {
 
   useEffect(() => {
     getSongPool()
+    socket = io('http://localhost:3001')
+    socket.on('pool update', pool => {
+      setPoolList(pool)
+    })
   }, [])
 
   const addSong = songData => {
-    console.log(songData.id);
-    let duplicate = poolList.some(song => song.id === songData.id)   //later on replace songData.id with the id of a song given by the Spotify API
-    if(!duplicate) {
-      setSearchResult(searchResult => searchResult.filter(song => song.id !== songData.id))
-
-      axios.post(baseUrl, songData)
-      .then(response => {
-        console.log(response)
-        getSongPool()
-      })
-    }
+    setSearchResult(searchResult => searchResult.filter(song => song.id !== songData.id))
+    socket.emit('song add', songData, error => {
+      if(error) {
+        alert(error)
+      }
+    })
   }
 
   function getSongPool(){
-    axios.get(baseUrl)
+    axios.get(baseUrl + 'pool')
     .then(response => {
-      response.data.sort(function(a,b) {
-        const voteA = a.voteCount
-        const voteB = b.voteCount
-        const dateA = a.dateAdded
-        const dateB = b.dateAdded
-        return (voteA > voteB) ? -1 : (voteA < voteB) ? 1 : (dateA > dateB) ? 1 : -1
-      })
       setPoolList(response.data)})
   }
   
   const search = input => {
-    console.log(input)
-    getSearchResult({id:"song-" + nanoid(), title:input, artist:"artist", duration:"1:11"})
-  }
-
-  const getSearchResult = songData => {
-    setSearchResult(searchResult => searchResult.concat(songData))
+    axios.get(baseUrl + `songs/${input}`)
+    .then(res => {
+      setSearchResult([res.data])
+    })
+    
   }
 
   const voteUpdate = (id,vote) =>{
-    const url = baseUrl + `/${id}`
-    console.log(url)
-    axios.patch(url, {vote})
-    .then(response =>{
-      getSongPool()
-    })
+    socket.emit('vote change', id, vote)
   }
 
   const poolComponentList = poolList.map(song => (
