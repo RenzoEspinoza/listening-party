dd73dab18c26bdb38a322f67a31279123ae4b44c
RenzoEspinoza <37278863+RenzoEspinoza@users.noreply.github.com>
2020-08-01 18:54:18 -0400

    uibuild

---
+++ backend/build/precache-manifest.a6e81b508dea233dc1ba6ce48a869b1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1091c9a8a4237f34ac10fadd216769c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eff491da1fa67a30aadb",
+    "url": "/static/css/main.cda36a3e.chunk.css"
+  },
+  {
+    "revision": "b661e2a2a02eda1ae66f",
+    "url": "/static/js/2.590aa123.chunk.js"
+  },
+  {
+    "revision": "91efb7b759cc0a54fac8775cb7b254e2",
+    "url": "/static/js/2.590aa123.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eff491da1fa67a30aadb",
+    "url": "/static/js/main.22f5cec9.chunk.js"
+  },
+  {
+    "revision": "815b9a1e94aa27f94eb2",
+    "url": "/static/js/runtime-main.502ec7f4.js"
+  }
+]);
